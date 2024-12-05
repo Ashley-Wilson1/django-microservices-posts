3b8d5be1279557a9d860e2323c1c923c603412f7
Ashley-Wilson1 <ashley03wilson@gmail.com>
2024-12-05 19:36:31 +0000

    Add the ability to ad post and comments using apis

--- comments/core/views.py
+++ comments/core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Comment
+from .serializers import CommentSerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+class PostCommentAPIView(APIView):
+    def get(sel, _, pk=None):
+        comments = Comment.objects.filter(post_id=pk)
+        serializer = CommentSerializer(comments,many=True)
+        return Response(serializer.data)
+
+class CommentsAPIView(APIView):
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
--- posts/core/views.py
+++ posts/core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+from .serializers import PostSerializer
+from .models import Post
+import requests
 
 # Create your views here.
+class PostAPIView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        
+        return Response([self.formatPost(p) for p in posts])
+    
+    def formatPost(self,post):
+        comments = requests.get('http://127.0.0.1:8001/api/posts/%d/comments' % post.id).json()
+        return{
+            'id':post.id,
+            'title':post.title,
+            'description':post.description,
+            'comments':comments
+        }
+
+    def post(self,request):
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
